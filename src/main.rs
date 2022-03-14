c2626238bed37c3521511ee1020e388e8d0c1940
Thi Dinh <vietthidinh2001@gmail.com>
2022-03-14 20:03:19 +0200

    feat: add GET route

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use rocket::tokio::select;
+use rocket::tokio::sync::broadcast::error::RecvError;
 use rocket::{
     form::Form,
+    response::stream::{Event, EventStream},
     routes,
     serde::{Deserialize, Serialize},
     tokio::sync::broadcast::{channel, Sender},
-    FromForm, State,
+    FromForm, Shutdown, State,
 };
 
 #[macro_use]
@@ -29,9 +32,29 @@ fn post(form: Form<Message>, queue: &State<Sender<Message>>) {
     let _res = queue.send(form.into_inner());
 }
 
+#[get("/events")]
+async fn events(queue: &State<Sender<Message>>, mut end: Shutdown) -> EventStream![] {
+    let mut rx = queue.subscribe();
+
+    EventStream! {
+        loop {
+            let msg = select!{
+                msg = rx.recv() => match msg {
+                    Ok(msg) => msg,
+                    Err(RecvError::Closed) => break,
+                    Err(RecvError::Lagged(_)) => continue,
+                },
+                _ = &mut end => break,
+            };
+
+            yield Event::json(&msg);
+        }
+    }
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .manage(channel::<Message>(1024).0)
-        .mount("/hello", routes![world, post])
+        .mount("/", routes![world, post, events])
 }
