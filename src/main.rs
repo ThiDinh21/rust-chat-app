f918ddf7799c4c7f1fd6c38883b492c5219b9cfa
Thi Dinh <vietthidinh2001@gmail.com>
2022-03-14 19:47:40 +0200

    feat: add POST route

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,37 @@
-use rocket::{routes, tokio::sync::broadcast::channel};
+use rocket::{
+    form::Form,
+    routes,
+    serde::{Deserialize, Serialize},
+    tokio::sync::broadcast::{channel, Sender},
+    FromForm, State,
+};
 
 #[macro_use]
 extern crate rocket;
+
+#[derive(Debug, Clone, FromForm, Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Message {
+    #[field(validate = len(..30))]
+    pub room: String,
+    #[field(validate = len(..20))]
+    pub username: String,
+    pub message: String,
+}
 
 #[get("/world")]
 fn world() -> &'static str {
     "Hello, world!"
 }
 
+#[post("/message", data = "<form>")]
+fn post(form: Form<Message>, queue: &State<Sender<Message>>) {
+    let _res = queue.send(form.into_inner());
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/hello", routes![world])
+    rocket::build()
+        .manage(channel::<Message>(1024).0)
+        .mount("/hello", routes![world, post])
 }
